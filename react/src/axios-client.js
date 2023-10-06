c22f6b9278f483205c54a76cd006100f5acd879e
suraj-singh12 <surajsingh5092@gmail.com>
2023-10-06 17:25:12 +0530

    request being made

--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -6,18 +6,18 @@ const axiosClient = axios.create({
 
 // Add a request interceptor (will be called before each request)
 axiosClient.interceptors.request.use((config) => {
-    const token = localStorage.get('ACCESS_TOKEN');
+    const token = localStorage.getItem('ACCESS_TOKEN');
     config.headers.Authorization = `Bearer ${token}`;
     return config;
 });
 
 
 // Add a response interceptor (will be called after each response, before sending to the calling component)
-axiosClient.interceptors.response.use(() => {
+axiosClient.interceptors.response.use((response) => {
     return response;
 }, (error) => {
     const {response} = error;
-    if(response.status == 401) {
+    if(response.status === 401) {
         // if user is unauthorized, remove the access token
         localStorage.removeItem('ACCESS_TOKEN');
     }
