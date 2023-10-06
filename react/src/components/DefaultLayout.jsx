6c336de3f5223f59b3ee371ff0c8006d0e90d64b
suraj-singh12 <surajsingh5092@gmail.com>
2023-10-06 14:50:12 +0530

    added contextProvider and css

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../contexts/ContextProvider'
 
 export default function DefaultLayout() {
+  const {user, token} = useStateContext();
+  if(!token) {
+    return <Navigate to="/login" />
+  }
+
   return (
-    <div>
+    <div id="defaultLayout">
         Default
         <Outlet />
     </div>
