9e596eed64a66dee18eb4642ffa0c043361e7f17
suraj-singh12 <surajsingh5092@gmail.com>
2023-10-06 15:43:08 +0530

    api calls to laravel backend

---
+++ react/src/axios-client.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const axiosClient = axios.create({
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
+})
+
+// Add a request interceptor (will be called before each request)
+axiosClient.interceptors.request.use((config) => {
+    const token = localStorage.get('ACCESS_TOKEN');
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+});
+
+
+// Add a response interceptor (will be called after each response, before sending to the calling component)
+axiosClient.interceptors.response.use(() => {
+    return response;
+}, (error) => {
+    const {response} = error;
+    if(response.status == 401) {
+        // if user is unauthorized, remove the access token
+        localStorage.removeItem('ACCESS_TOKEN');
+    }
+    throw error;
+})
+export default axiosClient;
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom';
+import axiosClient from '../axios-client';
+import { useStateContext } from '../contexts/ContextProvider';
 
 export default function Signup() {
+
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+    const {setUser, setToken} = useStateContext();
+
     const onSubmit = (ev) => {
         ev.preventDefault();
-      }
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        }
+        console.log(payload);
+
+        axiosClient.post('/signup', payload)
+        .then( ({data}) => {
+            setUser(data.user);
+            setToken(data.token);
+        })
+        .catch( err => {
+            const response = err.response;
+            if(response && response.status == 422) {
+                console.log(response.data.errors);
+            }
+        })
+    }
 
       return (
         <div className='login-signup-form animated fadeInDown'>
@@ -13,10 +41,10 @@ export default function Signup() {
                     <h1 className="title">
                         Signup for free
                     </h1>
-                    <input type="text" placeholder="Full Name" />
-                    <input type="email" placeholder="Email Address" />
-                    <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Password Confirmation" />
+                    <input type="text" ref={nameRef} placeholder="Full Name" />
+                    <input type="email" ref={emailRef} placeholder="Email Address" />
+                    <input type="password" ref={passwordRef} placeholder="Password" />
+                    <input type="password" ref={passwordConfirmationRef} placeholder="Password Confirmation" />
                     <button className="btn btn-block">Signup</button>
                     <p className="message">
                         Already Registered? <Link to="/login">Sign In</Link>
